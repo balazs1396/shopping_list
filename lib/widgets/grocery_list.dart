3025a55d8c2d3657842aa58690e0f6b81cff99ca
Balazs Szedlacsek <balazs.szedlacsek@alzura.com>
2025-08-22 14:50:59 +0200

    Fetches and displays grocery items from Firebase
    Refactors the grocery list to load items from a Firebase database.
    
    The initial hardcoded list is replaced with data fetched from the database
    on initialization and after adding a new item. This allows for persistent
    storage and retrieval of grocery items.

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -13,20 +18,50 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = groceryItems.toList();
+  final List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    _groceryItems.clear();
+    final url = Uri.https(
+      'flutter-practice-6f92f-default-rtdb.europe-west1.firebasedatabase.app',
+      'shopping-list.json',
+    );
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+
+    for (final item in listData.entries) {
+      final category =
+          categories.entries
+              .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'],
+              )
+              .value;
+
+      _groceryItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    setState(() {});
+  }
 
   void _onAddItem() async {
-    final newItem = await Navigator.of(
+    await Navigator.of(
       context,
-    ).push<GroceryItem>(MaterialPageRoute(builder: (ctx) => const NewItem()));
+    ).push(MaterialPageRoute(builder: (ctx) => const NewItem()));
 
-    if (newItem == null) {
-      return;
-    }
-
-    setState(() {
-      _groceryItems.add(newItem);
-    });
+    _loadItems();
   }
 
   @override
@@ -58,7 +93,10 @@ class _GroceryListState extends State<GroceryList> {
                 SnackBar(content: Text('${currentItem.name} dismissed')),
               );
             },
-            background: Container(color: Colors.red, child: Text('Removing...'),),
+            background: Container(
+              color: Colors.red,
+              child: Text('Removing...'),
+            ),
             child: ListTile(
               title: Text(currentItem.name),
               leading: Container(
